dbce1a902944481c7903c624e443dc552759034e
Ac6 <38214010+RoyAc6@users.noreply.github.com>
2025-10-17 02:54:18 +0200

    fix the name of the jlink tool as it is not the same on windows or linux

--- src/debug/runners/JLink.ts
+++ src/debug/runners/JLink.ts
@@ -9,7 +9,7 @@ export class JLink extends WestRunner {
   get executable(): string | undefined {
     const exec = super.executable;
     if(!exec) {
-      return 'JLinkGDBServerCL';
+      return process.platform === 'win32' ? 'JLinkGDBServerCL' : 'JLinkGDBServerCLExe';
     }
   }
 
